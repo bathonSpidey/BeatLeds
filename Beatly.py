96501407e32b7263cb9db20e8854ed674a1bed67
Abir Bhattacharyya <46240907+bathonSpidey@users.noreply.github.com>
2020-12-26 18:03:53 +0100

    Code refactored and tested more
    Still need to figure out how to suppress errors and in the dead thread. Very ugly console output

--- Beatly.py
+++ Beatly.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Dec 24 09:31:41 2020
-@author: batho
+@author: spidey
 """
 
 import pyaudio
@@ -9,18 +9,15 @@ import numpy as np
 import requests as req
 from time import sleep
 import threading
-import warnings
-warnings.filterwarnings("ignore")
-np.set_printoptions(suppress=True) # don't use scientific notation
 
-CHUNK = 1024*2 # number of data points to read at a time
-RATE = 44100 # time resolution of the recording device (Hz)
-TARGET = 2100 # show only this one frequency
+
+CHUNK = 1024*2 
+RATE = 44100 
+TARGET = 2100 
+REST=.03
 p=pyaudio.PyAudio() # start the PyAudio class
 stream=p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
-              frames_per_buffer=CHUNK) #uses default input device
-
-# create a numpy array holding a single read of audio data
+              frames_per_buffer=CHUNK) 
 change=0
 red=np.random.randint(255)
 green=np.random.randint(255)
@@ -40,35 +37,39 @@ def threadStrip(ledNumbers,red, green, blue):
     t = threading.Thread(target=callStrip, args=(ledNumbers,red, green, blue))
     t.daemon = True
     t.start()
-try:
-    while True:
-        if change%500:
-            red=np.random.randint(255)
-            green=np.random.randint(255)
-            blue=np.random.randint(255)
-        change+=1
-        data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
-        peak=np.average(np.abs(data))*2
-        fft = abs(np.fft.fft(data).real)
-        fft = fft[:int(len(fft)/2)] # keep only first half
-        freq = np.fft.fftfreq(CHUNK,1.0/RATE)
-        freq = freq[:int(len(freq)/2)] # keep only first half
-        assert freq[-1]>TARGET, "ERROR: increase chunk size"
-        val = fft[np.where(freq>TARGET)[0][0]]
-        ledNumbers=int(np.interp(val/10,[0,800], [0,30]))
-        
-        if ledNumbers < 2 and (5<= peak <60) and 10<(prevPeak-peak)<30:
-            threadRainbow()
-        else:
-            threadStrip(ledNumbers,red, green, blue)
-        prevPeak=peak
-        #print(str(ledNumbers)+":"+str(val/10)+":"+ int(val/10)*"*")
-   
-except KeyboardInterrupt:
-# close the stream gracefully
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-stream.stop_stream()
-stream.close()
-p.terminate()
+def getTriggerParameters(stream):
+    data = np.fromstring(stream.read(CHUNK),dtype=np.int16)
+    peak=np.average(np.abs(data))*2
+    fft = abs(np.fft.fft(data).real)
+    fft = fft[:int(len(fft)/2)] # keep only first half
+    freq = np.fft.fftfreq(CHUNK,1.0/RATE)
+    freq = freq[:int(len(freq)/2)] # keep only first half
+    assert freq[-1]>TARGET, "ERROR: increase chunk size"
+    val = fft[np.where(freq>TARGET)[0][0]]/25
+    return val, peak
+def getColors():
+    return np.random.randint(255),np.random.randint(255),np.random.randint(255)
+def changeNeoPixel(ledNumbers, peak, prevPeak):
+    if ledNumbers < 2 and (5<= peak <60) and 10<(prevPeak-peak)<25:
+        threadRainbow()
+        sleep(REST)
+    else:
+        threadStrip(ledNumbers,red, green, blue)
+        sleep(REST)
+    
+if __name__=="__main__":
+    try:
+        while True:
+            if change%500:
+                red, green, blue = getColors()
+            change+=1
+            val, peak=getTriggerParameters(stream)
+            ledNumbers=int(np.interp(val,[0,1250], [0,30])*1.2)
+            changeNeoPixel(ledNumbers, peak, prevPeak)
+            prevPeak=peak
+            #print(str(ledNumbers)+":"+str(val/10)+":"+ int(val/10)*"*") 
+    except KeyboardInterrupt:
+    # close the stream gracefully
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
